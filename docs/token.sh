48294ea45fecbeb4a5b995b02a77f45d15f704f8
dust <dust@kmfrog.com>
2023-10-13 11:12:41 +0800

    snapshot-alpha-1024

---
+++ docs/token.sh
@@ -0,0 +1,12 @@
+export account=account_tdx_2_129th30gyg5w0fh06swecmtg6ddcqfl77qme7ffvqzrgwc7kyelr5tp
+export name="Dust"
+export description="Dust's token"
+export symbol="DST"
+export icon_url="https://docs.dexian.io/assets/images/dexian.png"
+export info_url="https://dexian.io/"
+export dapp_definition="${account}"
+export supply=1000
+export tag1=dev
+export tag2=scrypto
+
+./docs/replace_holder.sh ./docs/transactions/create_token.rtm
